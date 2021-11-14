006287800885907c74c3c3eb68b9e3e78b84c8d5
calvin <cal.villa@icloud.com>
2021-11-14 15:12:44 -0500

    started setting up JS

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var startButton = document.querySelector('#start')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    
+}
+
+function nextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
