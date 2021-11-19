9b82962b1cba06863bbfb3cdde0e3baa0a92bf32
calvin <cal.villa@icloud.com>
2021-11-18 23:02:41 -0500

    started JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,41 @@
-var startButton = document.querySelector('#start')
+const question = document.querySelector('question');
+const choices = document.querySelectorAll('.choice-text');
 
-startButton.addEventListener('click', startGame)
+let currentQuestion = {}
+let acceptingAnswers = true
 
-function startGame() {
-    
-}
-
-function nextQuestion() {
-
-}
-
-function selectAnswer() {
-
-}
+let questions = [
+    {
+        question: "Commonly used data types DO Not Include:",
+        choice1: "strings",
+        choice2: "booleans",
+        choice3: "alerts",
+        choice4: "numbers",
+        answer: alerts,
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ________.",
+        choice1: "numbers and strings",
+        choice2: "other arrays",
+        choice3: "booleans",
+        choice4: "all of the above",
+        answer: all of the above,
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables",
+        choice1: "commas",
+        choice2: "curly brackets",
+        choice3: "quotes",
+        choice4: "parenthesis",
+        answer: curly brackets,
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choice1: "JavaScript",
+        choice2: "terminal/bash",
+        choice3: "for loops",
+        choice4: "console.log",
+        answer: alerts
+    },
+]
 
